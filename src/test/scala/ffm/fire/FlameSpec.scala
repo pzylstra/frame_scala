a85655f90319d3b206aaa084827921be8db397d6
mbedward <michael.bedward@gmail.com>
2014-09-20 12:38:43 +1000

    Flame unit tests.

--- src/test/scala/ffm/fire/FlameSpec.scala
+++ src/test/scala/ffm/fire/FlameSpec.scala
@@ -76,6 +76,26 @@ class FlameSpec extends BasicSpec with PropertyChecks with OptionValues {
     }
   }
   
+  it should "return plume temperature equal to plume delta temperature plus ambient" in {
+    val depthIgnited = 0.5
+
+    // Although it's unlikely fires will be modelled in sub-zero temperatures
+    // we want to exercise the code here
+    val ambientTemps = Gen.choose(-20.0, 40.0)
+    val flameTemps = Gen.choose(200.0, 300.0)
+    
+    forAll (ambientTemps, flameTemps, distances) { (ambientT, flameT, d) => 
+      whenever (d > depthIgnited) {
+        val flame = Flame(depthIgnited * 2, 0.0, Coord.Origin, depthIgnited, flameT)
+        val pdt = flame.plumeDeltaTemperature(d)
+        val pt = flame.plumeTemperature(d, ambientT)
+        
+        pt should be (pdt + ambientT)
+      } 
+    }
+  }
+  
+  
   it should "return the flame temperature for distances <= depth ignited and lower temperatures otherwise" in {
     val depthIgnited = XLim / 2
     val flameLength = depthIgnited + 1.0
@@ -96,6 +116,20 @@ class FlameSpec extends BasicSpec with PropertyChecks with OptionValues {
     
     flame.distanceForTemperature(t + 21.0, 20.0) should be (None)
   }
+  
+  it should "return depth ignited for the distance to the flame temperature" in {
+    val flameTemp = 300.0
+    val ambientTemp = 20.0
+    
+    forAll (distances, angles) { (depth, angle) =>
+      whenever (depth > 0.1) {
+        val flameLength = depth + 1.0
+        val flame = Flame(flameLength, angle, Coord.Origin, depth, flameTemp)
+        
+        flame.distanceForTemperature(flameTemp + ambientTemp, ambientTemp).value should be (depth)
+      }
+    }
+  }
 
   it should "calculate distanceForTemperature values which agree with plumeTemperature values" in {
     val depthIgnited = XLim / 10
@@ -105,8 +139,13 @@ class FlameSpec extends BasicSpec with PropertyChecks with OptionValues {
 
     val flame = Flame(flameLength, 0.0, Coord.Origin, depthIgnited, flameTemp)
 
-    // For random distances > depthIgnited, calculate temperature at that distance and then
-    // check the inverse calculation of distance for that temperature
+    /*
+     * For random distances > depthIgnited, calculate temperature at that distance and then
+     * check the inverse calculation of distance for that temperature.
+     * 
+     * We restrict the test to distances > depthIgnited because flame.plumeTemperature simply
+     * returns the flame temperature for smaller distances.
+     */
     forAll(distances) { d =>
       whenever(d > depthIgnited) {
         val calculatedTemp = flame.plumeTemperature(d, ambientTemp)
