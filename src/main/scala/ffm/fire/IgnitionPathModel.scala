4c7cf5d5bb604d989521ee87f90a337f435e7cee
mbedward <michael.bedward@gmail.com>
2015-02-02 10:34:38 +1100

    Separate file for IgnitionContext

--- src/main/scala/ffm/fire/IgnitionPathModel.scala
+++ src/main/scala/ffm/fire/IgnitionPathModel.scala
@@ -1,28 +1,9 @@
 package ffm.fire
 
-import ffm.forest.Site
 import ffm.forest.SpeciesComponent
-import ffm.forest.StratumLevel
 import ffm.geometry.Coord
 
 trait IgnitionPathModel {
   def generatePath(context: IgnitionContext, plantFlameModel: PlantFlameModel)(speciesComponent: SpeciesComponent, intialPoint: Coord): IgnitionPath
 }
 
-sealed trait IgnitionRunType
-object IgnitionRunType {
-  case object PlantRun extends IgnitionRunType
-  case object StratumRun extends IgnitionRunType
-}
-
-case class IgnitionContext(
-  runType: IgnitionRunType,
-  site: Site,
-  stratumLevel: StratumLevel,
-  preHeatingFlames: IndexedSeq[PreHeatingFlame],
-  incidentFlames: IndexedSeq[Flame],
-  preHeatingEndTime: Double,
-  canopyHeatingDistance: Double,
-  stratumWindSpeed: Double)
-
-  
